22086ac5ca6abe822c5987c5646e0750e07ff1f0
Gizem Izmir <gizkes1996@gmail.com>
2022-06-05 20:41:34 +0300

    Selenium test

---
+++ tests/test.js
@@ -0,0 +1,35 @@
+const {By,Key,Builder} = require("selenium-webdriver");
+require("chromedriver");
+ 
+async function example(){
+ 
+       var searchString = "En uygun";
+ 
+       //To wait for browser to build and launch properly
+       let driver = await new Builder().forBrowser("chrome").build();
+ 
+        //To fetch http://google.com from the browser with our code.
+
+        await driver.get("http://google.com");
+
+            
+        //To send a search query by passing the value in searchString.
+        await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
+ 
+        //Verify the page title and print it
+        var title = await driver.getTitle();
+
+        if (title === "En uygun - Google'da Ara") {
+            console.log('Title is:',title);
+            console.log('Test Başarılı');
+        } else {
+            console.log('Title is:',title);
+            console.log('Test Başarısız');
+        }
+ 
+        //It is always a safe practice to quit the browser after execution
+        await driver.quit();
+ 
+}
+ 
+example()
